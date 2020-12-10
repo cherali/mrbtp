8ddff7a8aefd64759ce48103fa1c98a2c8b291ce
ali cheraghi <alicheraghi148@gmail.com>
2020-12-10 15:09:02 +0330

    add colors

---
+++ common/mainStyle/theme.js
@@ -0,0 +1,10 @@
+export const primaryColor = '#f0b90b'
+export const successColor = '#02c076'
+export const redColor = '#f84960'
+
+export const garyColor = '#aeb4bc'
+export const lightGrayColor = '#76808f'
+export const darkGrayColor = '#212833'
+
+
+export const darkColor = '#1e2026'
